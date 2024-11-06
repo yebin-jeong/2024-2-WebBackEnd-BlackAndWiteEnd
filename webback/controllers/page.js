0ea41b28fbdf8b4924f8c8b51237874d75ab39ca
Minbro-Kim <ab000701@dgu.edu>
2024-11-06 17:05:47 +0900

    BE:[feat] 로그인, 회원가입

---
+++ webback/controllers/page.js
@@ -0,0 +1,12 @@
+exports.renderProfile =(req, res)=>{
+    res.render('profile', {title: '내정보 - 동국요리사'});
+};
+
+exports.renderJoin =(req, res)=>{
+    res.render('join', {title: '회원가입 - 동국요리사'});
+};
+
+exports.renderMain =(req, res,next)=>{
+    const twits = [];
+    res.render('main', {title: '동국요리사', twits});
+};
---
+++ webback/models/user.js
@@ -0,0 +1,50 @@
+const Sequelize = require('sequelize');
+
+module.exports = class User extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init({
+      email: {
+        type: Sequelize.STRING(40),
+        allowNull: false,
+        unique: true,
+      },
+      nick: {
+        type: Sequelize.STRING(15),
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+      },
+      provider: {
+        type: Sequelize.STRING(10),
+        allowNull: false,
+        defaultValue: 'local',
+      },
+      
+    }, {
+      sequelize,
+      timestamps: true,
+      underscored: false,
+      modelName: 'User',
+      tableName: 'users',
+      paranoid: true,
+      charset: 'utf8',
+      collate: 'utf8_general_ci',
+    });
+  }
+
+  static associate(db) {
+    db.User.hasMany(db.Post);
+    db.User.belongsToMany(db.User, {
+      foreignKey: 'followingId',
+      as: 'Followers',
+      through: 'Follow',
+    });
+    db.User.belongsToMany(db.User, {
+      foreignKey: 'followerId',
+      as: 'Followings',
+      through: 'Follow',
+    });
+  }
+};
