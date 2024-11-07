817b6b6768befca52fb660bd61d3eae699e2614e
Minbro-Kim <ab000701@dgu.edu>
2024-11-07 20:27:39 +0900

    BE:[refactor] 로그인 페이지 분리

--- webback/controllers/page.js
+++ webback/controllers/page.js
@@ -10,3 +10,6 @@ exports.renderMain =(req, res,next)=>{
     const twits = [];
     res.render('main', {title: '동국요리사', twits});
 };
+exports.renderLogin =(req, res,next)=>{
+    res.render('login', {title: '로그인'});
+};
