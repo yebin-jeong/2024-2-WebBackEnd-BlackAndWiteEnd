2d1894a12387eae53fbc81c23da34983373aa629
Minbro-Kim <ab000701@dgu.edu>
2024-11-09 15:05:43 +0900

    BE:[feat] 찜 개수

--- webback/controllers/restaurant.js
+++ webback/controllers/restaurant.js
@@ -10,14 +10,21 @@ exports.renderResList = async(req, res)=>{
 
     try {
         const dis = req.params.district; // URL 파라미터로 지역(district) 받기
-        const rest = await Restaurant.findAll({
+        const resList = await Restaurant.findAll({
             where: { district: dis}
         });       
 
+        // 각 레스토랑의 찜 개수 계산
+    for (const restaurant of resList) {
+      const followCount = await restaurant.countFollowers();
+      restaurant.followCount = followCount; // 레스토랑 객체에 찜 개수 추가
+    }
+
         res.render('resList', {
             title: districtMap[dis],
-            restaurants: rest
+            restaurants: resList
         });
+
     } catch(error) {
         console.error(error);
         res.status(500).send('음식점 데이터를 가져오는 데 실패했습니다.');
@@ -30,8 +37,11 @@ exports.renderDetail =async(req, res)=>{
     const resId = parseInt(req.params.id); // URL에서 ID 파라미터 추출
     try {
         const restaurant = await Restaurant.findByPk(resId);
+        const followCount = await restaurant.countFollowers(); // 찜 개수 가져오기
+        restaurant.favorite_count = followCount;
+
         res.render('detail', {
-            res: restaurant
+            res: restaurant,
         });
     }catch(error) {
         console.error(error);
