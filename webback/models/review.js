936d9c6db305f2f15668a5f015ae167fb8bcfd96
Juri-Jang <jjr84@dgu.ac.kr>
2024-11-09 20:44:53 +0900

    BE:[fix]충돌해결

---
+++ webback/models/review.js
@@ -0,0 +1,37 @@
+const Sequelize = require('sequelize');
+
+module.exports = class Review extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init({
+      restaurantId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      content: {
+        type: Sequelize.STRING(140),
+        allowNull: false,
+      },
+      rating: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+    }, {
+      sequelize,
+      timestamps: true,
+      underscored: false,
+      modelName: 'Review',
+      tableName: 'reviews',
+      paranoid: false,
+      charset: 'utf8mb4',
+      collate: 'utf8mb4_general_ci',
+    });
+  }
+
+  static associate(db) {
+    db.Review.belongsTo(db.User);
+    db.Review.belongsTo(db.Restaurant, {
+      foreignKey: 'restaurantId',  // Review 모델에서 사용할 외래 키
+      as: 'restaurant'  // 이 관계를 조회할 때 사용할 이름
+  });
+  }
+};
